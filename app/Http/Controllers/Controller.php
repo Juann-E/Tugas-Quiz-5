1fca18ceb2b408bef7274dc45724589fe7bf0c26
aryakk11 <aryaabdilah95@gmail.com>
2026-05-12 17:51:57 +0700

    Quiz 5 - 672022303

--- app/Http/Controllers/Controller.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Http\Controllers;
-
-abstract class Controller
-{
-    // WAJIB KOSONG SEPERTI INI.
-    // Jika diisi logika lain, VS Code akan merah semua seperti di screenshot Anda.
-}
